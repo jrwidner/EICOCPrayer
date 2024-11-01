73aa2463d559fc913f4591ab277ea57bf002db38
Jeremy Widner <144156667+jrwidner@users.noreply.github.com>
2024-11-01 13:06:25 -0500

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -103,7 +103,7 @@ router.post('/upload', upload.single('file'), async (req, res) => {
         res.send(response.data);
     } catch (error) {
         res.status(500).json({ error: `${error.message} - URL: UPLOAD_ATTENDANCE`,
-                             data: {records});
+                             data: '{records}');
     }
 });
 
